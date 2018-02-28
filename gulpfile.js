d06c2ccc37bc997785040c1e39c5a64d281840f3
javatmp <support@javatmp.com>
2018-02-28 14:21:09 +0400

    add gulp-header plugin and update version prior to publish

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,18 @@ var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var del = require('del');
+var header = require('gulp-header');
+var pkg = require('./package.json');
+
+var banner = ['/*!',
+    ' * <%= pkg.name %> <%= pkg.version %> (http://javatmp.com)',
+    ' * <%= pkg.description %>',
+    ' *',
+    ' * @link <%= pkg.homepage %>',
+    ' * @copyright 2018 JavaTMP',
+    ' * @license <%= pkg.license %>',
+    ' */',
+    ''].join('\n');
 
 gulp.task('delete-css', function () {
     return del(['./dist/**/*']);
@@ -22,6 +34,7 @@ gulp.task('sass', ["delete-css"], function () {
                 browsers: ['last 2 versions'],
                 cascade: false
             }))
+            .pipe(header(banner, {pkg: pkg}))
             .pipe(cleanCSS())
             .pipe(rename({suffix: '.min'}))
             .pipe(gulp.dest('./dist/'));
